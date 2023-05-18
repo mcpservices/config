98ff2e0c022f00af7a029ea3cfd77072ae058f94
Samuel Lotfi <samuel.lotfi@mclaren.com>
2023-05-18 12:12:52 +0100

    a2

--- copy.sh
+++ copy.sh
@@ -6,13 +6,23 @@ cd "$(dirname "$0")"
 # Source file paths
 ap_file="ap"
 hostapd_conf_file="hostapd.conf"
+isc_config="isc-dhcp-server"
+dhcp_config="dhclient.conf"
 
 # Destination directories
-interfaces_dir="/root/"
-hostapd_dir="/root/"
+interfaces_dir="/etc/network/interfaces.d/"
+hostapd_dir="/etc/hostapd/"     
+isc_dir="/etc/default/"
+dhcp_dir="/etc/"
 
 # Copy ap file to /etc/network/interfaces/
 cp "$ap_file" "$interfaces_dir"
 
 # Copy hostapd.conf file to /etc/hostapd/
 cp "$hostapd_conf_file" "$hostapd_dir"
+
+#copy isc
+cp "$isc_config" "$isc_dir"
+
+#copy dhcp
+cp "$dhcp_config" "$dhcp_dir"
